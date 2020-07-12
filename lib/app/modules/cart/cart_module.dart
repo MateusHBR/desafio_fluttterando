2fb422ea6c0e4ae1c4ba3ebcc31a0d1de2990394
MateusHBR <mateusmorais.r@gmail.com>
2020-07-12 14:43:02 -0300

    injetando dependencia no controller do carrinho

--- lib/app/modules/cart/cart_module.dart
+++ lib/app/modules/cart/cart_module.dart
@@ -1,12 +1,20 @@
+import 'package:desafio_flutterando_coffe/app/shared/stores/coffe_store.dart';
+
+import 'cart_controller.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:dio/dio.dart';
+import 'cart_page.dart';
 
 class CartModule extends ChildModule {
   @override
-  List<Bind> get binds => [];
+  List<Bind> get binds => [
+        Bind((i) => CartController(i.get<CoffeStore>())),
+      ];
 
   @override
-  List<Router> get routers => [];
+  List<Router> get routers => [
+        Router(Modular.initialRoute, child: (_, args) => CartPage()),
+      ];
 
   static Inject get to => Inject<CartModule>.of();
 }
