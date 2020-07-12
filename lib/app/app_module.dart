76234291c22d210873e12ccd5631a1231a08d250
MateusHBR <mateusmorais.r@gmail.com>
2020-07-12 12:51:24 -0300

    adicionando rota

--- lib/app/app_module.dart
+++ lib/app/app_module.dart
@@ -1,3 +1,6 @@
+import 'package:desafio_flutterando_coffe/app/modules/item/item_module.dart';
+import 'package:desafio_flutterando_coffe/app/shared/stores/coffe_store.dart';
+
 import 'app_controller.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter/material.dart';
@@ -8,11 +11,13 @@ class AppModule extends MainModule {
   @override
   List<Bind> get binds => [
         Bind((i) => AppController()),
+        Bind((i) => CoffeStore()),
       ];
 
   @override
   List<Router> get routers => [
         Router(Modular.initialRoute, module: HomeModule()),
+        Router('/item', module: ItemModule()),
       ];
 
   @override
