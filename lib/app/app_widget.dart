0cf35038740e2639ff3ac9374fc066c96db16508
MateusHBR <mateusmorais.r@gmail.com>
2020-07-12 12:52:16 -0300

    criando um  scaffold global  para snackbar  global

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import './shared/utils/global_scaffold.dart';
+// import 'package:asuka/asuka.dart' as asuka;
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      builder: (context, child) {
+        return Scaffold(
+          key: GlobalScaffold.instance.scaffKey,
+          body: child,
+        );
+      },
+      // builder: asuka.builder,
       navigatorKey: Modular.navigatorKey,
       title: 'Flutter Slidy',
       theme: ThemeData(
