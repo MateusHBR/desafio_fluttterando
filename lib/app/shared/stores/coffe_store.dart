298397cb8cb885c5c7b838d82799f3486532815d
MateusHBR <mateusmorais.r@gmail.com>
2020-07-12 12:55:44 -0300

    funcionalidades para adicionar e   remover do carrinho

--- lib/app/shared/stores/coffe_store.dart
+++ lib/app/shared/stores/coffe_store.dart
@@ -1,6 +1,32 @@
+import 'package:desafio_flutterando_coffe/app/shared/models/coffe_model.dart';
+import 'package:desafio_flutterando_coffe/app/shared/models/cofffe_cart_model.dart';
 import 'package:mobx/mobx.dart';
 part 'coffe_store.g.dart';
 
 class CoffeStore = _CoffeStoreBase with _$CoffeStore;
 
-abstract class _CoffeStoreBase with Store {}
+abstract class _CoffeStoreBase with Store {
+  @observable
+  ObservableList<CoffeCartModel> cart = <CoffeCartModel>[].asObservable();
+
+  @action
+  addCoffe(CoffeModel coffe) {
+    int index = cart.indexWhere((coffeCart) => coffeCart.coffe.id == coffe.id);
+
+    if (index < 0) {
+      cart.add(CoffeCartModel(coffe: coffe, quantity: 1));
+    } else {
+      cart[index] = cart[index].copyWith(quantity: cart[index].quantity++);
+    }
+  }
+
+  removeCoffe(CoffeModel coffe) {
+    int index = cart.indexWhere((coffeCart) => coffeCart.coffe.id == coffe.id);
+
+    if (cart[index].quantity > 1) {
+      cart[index] = cart[index].copyWith(quantity: cart[index].quantity--);
+    } else {
+      cart.removeAt(index);
+    }
+  }
+}
--- lib/app/shared/stores/coffe_store.g.dart
+++ lib/app/shared/stores/coffe_store.g.dart
@@ -9,10 +9,39 @@ part of 'coffe_store.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
 
 mixin _$CoffeStore on _CoffeStoreBase, Store {
+  final _$cartAtom = Atom(name: '_CoffeStoreBase.cart');
+
+  @override
+  ObservableList<CoffeCartModel> get cart {
+    _$cartAtom.reportRead();
+    return super.cart;
+  }
+
+  @override
+  set cart(ObservableList<CoffeCartModel> value) {
+    _$cartAtom.reportWrite(value, super.cart, () {
+      super.cart = value;
+    });
+  }
+
+  final _$_CoffeStoreBaseActionController =
+      ActionController(name: '_CoffeStoreBase');
+
+  @override
+  dynamic addCoffe(CoffeModel coffe) {
+    final _$actionInfo = _$_CoffeStoreBaseActionController.startAction(
+        name: '_CoffeStoreBase.addCoffe');
+    try {
+      return super.addCoffe(coffe);
+    } finally {
+      _$_CoffeStoreBaseActionController.endAction(_$actionInfo);
+    }
+  }
+
   @override
   String toString() {
     return '''
-
+cart: ${cart}
     ''';
   }
 }
